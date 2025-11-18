d3ee1d80bceb0009da75d5cfa9317c6f2b88234d
projetos-logistica <projetos.logistica@somagrupo.com.br>
2025-11-18 16:46:49 -0300

    Inlcuido um novo Usuario
    Usuarios Recebimento 2°T

--- cadastro_hc.py
+++ cadastro_hc.py
@@ -67,7 +67,10 @@ ALLOWED_EMAILS_DEFAULT = {
   "carlos.desouza@somagrupo.com.br",
   "natanael.junior@somagrupo.com.br", 
   "luiz.silva@somagrupo.com.br", 
-  "alessandro.jorge@farmrio.com.br", #usuário comum (sem admin)
+  "alessandro.jorge@farmrio.com.br",
+  "eduardo.oliveira@somagrupo.com.br", 
+  "allan.pires@somagrupo.com.br", 
+  "marlon.freitas@somagrupo.com.br",#usuário comum (sem admin)
 }
 
 ADMIN_EMAILS = {
